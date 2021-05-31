39fd533b81e637ce9c56e7c33ed690147d6e7b07
23prime <23.prime.37@gmail.com>
2021-05-31 20:57:02 +0900

    Add volumes for save build time

--- notification-by-gokabot/build.sh
+++ notification-by-gokabot/build.sh
@@ -8,7 +8,6 @@ docker compose run -f docker-compose.build.yml --rm notification-by-gokabot-buil
 docker compose build -f docker-compose.yml
 
 # Push
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 678084882233.
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 678084882233.dkr.ecr.ap-northeast-1.amazonaws.com
 docker tag notification-by-gokabot:latest 678084882233.dkr.ecr.ap-northeast-1.amazonaws.com/notification-by-gokabot:latest
-dkr.ecr.ap-northeast-1.amazonaws.com
 docker push 678084882233.dkr.ecr.ap-northeast-1.amazonaws.com/notification-by-gokabot:latest
