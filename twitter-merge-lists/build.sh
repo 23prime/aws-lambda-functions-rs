de599ae3017156e78aca6a671fa1b554d9a1e224
23prime <23.prime.37@gmail.com>
2022-04-08 00:04:49 +0900

    Fix variables definition

--- twitter-merge-lists/build.sh
+++ twitter-merge-lists/build.sh
@@ -3,8 +3,6 @@
 # Set variables
 DOCKER_IMAGE_NAME=twitter-merge-lists
 DOCKER_BUILD_CONTAINER_NAME=twitter-merge-lists-build
-AWS_ACCOUNT_ID=678084882233
-AWS_REGION=ap-northeast-1
 
 # Build container for musl build
 docker-compose -f docker-compose.build.yml build
