05e028e968bf0e0a09d7b72b6c33d2ba6680a642
23prime <23.prime.37@gmail.com>
2022-05-01 14:31:15 +0900

    Implement fetching XML from tonarinoyj.jp

--- tonarinoyj-update-checker/src/lib.rs
+++ tonarinoyj-update-checker/src/lib.rs
@@ -1,3 +1,6 @@
+use log::{error, info};
+use std::env;
+
 use dotenv::dotenv;
 
 type BoxError = Box<dyn std::error::Error>;
@@ -8,5 +11,23 @@ pub mod logger;
 
 pub async fn run() -> Result<(), BoxError> {
     dotenv().ok();
+
+    let series = fetch_series().await?;
+    info!("{:?}", series);
+
     return Ok(());
 }
+
+async fn fetch_series() -> Result<String, BoxError> {
+    let entry_id = env::var("TYJ_ENTRY_ID").expect("TYJ_ENTRY_ID must be set");
+    let url = format!("https://tonarinoyj.jp/atom/series/{}", entry_id);
+    let response = reqwest::get(url).await?;
+
+    if !response.status().is_success() {
+        error!("Failed to get series from tonarinoyj.jp: {:?}", response);
+        return Err(Box::new(error::LambdaGeneralError::none()));
+    }
+
+    return Ok(response.text().await?);
+}
+ 
