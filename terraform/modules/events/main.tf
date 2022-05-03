26ceb793f3c3e900f1ea62c1b8487605038fce96
23prime <23.prime.37@gmail.com>
2022-05-03 14:52:40 +0900

    Simplify comments

--- terraform/modules/events/main.tf
+++ terraform/modules/events/main.tf
@@ -1,8 +1,8 @@
+# See cron settings: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
 resource "aws_cloudwatch_event_rule" "twitter-followee-list-schedule" {
   name        = "twitter-followee-list-schedule"
   description = "Scheduled event for Lambda function: twitter-followee-list"
 
-  # cron: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
   schedule_expression = "cron(0 * * * ? *)"
 
   tags = {
@@ -20,7 +20,6 @@ resource "aws_cloudwatch_event_rule" "twitter-merge-vtubers-lists-schedule" {
   name        = "twitter-merge-vtubers-lists-schedule"
   description = "Scheduled event for Lambda function: twitter-merge-vtubers-lists"
 
-  # cron: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
   schedule_expression = "cron(0 * * * ? *)"
 
   tags = {
@@ -38,7 +37,6 @@ resource "aws_cloudwatch_event_rule" "one-punch-man-update-checker-schedule" {
   name        = "one-punch-man-update-checker-schedule"
   description = "Scheduled event for Lambda function: one-punch-man-update-checker"
 
-  # cron: https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html
   schedule_expression = "cron(0 * * * ? *)"
 
   tags = {
