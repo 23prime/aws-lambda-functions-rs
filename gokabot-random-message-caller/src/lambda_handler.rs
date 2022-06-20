71f486fe06b4b0874e2332cd08d5942012d56683
23prime <23.prime.37@gmail.com>
2022-06-21 01:02:23 +0900

    Use `join_all` provided by futures crate for async multi threading

--- gokabot-random-message-caller/src/lambda_handler.rs
+++ gokabot-random-message-caller/src/lambda_handler.rs
@@ -1,3 +1,4 @@
+use futures::future::join_all;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use log::info;
 
@@ -21,18 +22,12 @@ async fn handler(
 ) -> Result<(), error::LambdaGeneralError> {
     info!("event: {:?}", event);
 
-    let mut has_error = false;
-
     let target_ids = vec!["MY_USER_ID", "NGA_GROUP_ID", "KMT_GROUP_ID"];
 
-    for target_id in target_ids {
-        let result = gokabot::call(target_id).await;
-        info!("Call gokabot API [{}] => {:?}", target_id, result);
+    let future_tasks = target_ids.into_iter().map(gokabot::call);
+    let result = join_all(future_tasks).await;
 
-        if result.is_err() {
-            has_error = true;
-        }
-    }
+    let has_error = result.into_iter().any(|r| r.is_err());
 
     if has_error {
         return Err(error::LambdaGeneralError::new(
